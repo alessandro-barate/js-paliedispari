b5303272bacc2cc5e093e1387b3b3a0477b730ec
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2024-05-07 21:08:45 +0200

    [UPD] Even odd exercise - working on the logic

--- pari-e-dispari/js/main.js
+++ pari-e-dispari/js/main.js
@@ -1,1 +1,14 @@
 "use strict";
+
+const userEvenOdd = prompt("Scegli pari o dispari");
+const userNumber = Number(prompt("Scegli un numero da 1 a 5"));
+const pcNumber = randomNumber();
+
+function randomNumber() {
+  let random = Math.floor(Math.random() * 5 + 1);
+  return random;
+}
+
+console.log(userEvenOdd);
+console.log(userNumber);
+console.log(pcNumber);
