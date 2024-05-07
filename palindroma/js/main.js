7dd0890ce611408ef26b92a7ed5ccd36de40a3ba
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2024-05-07 19:22:01 +0200

    [UPD] Palindrome exercise - lower case control added

--- palindroma/js/main.js
+++ palindroma/js/main.js
@@ -3,13 +3,11 @@
 // Chiedo all'utente di inserire una parola
 const userInput = prompt("Inserisci una parola");
 
-//console.log(userInput);
+let userInputLowerCase = userInput.toLowerCase();
 
 function palindrome() {
   // Divido la parola nelle sue lettere e vengono messe in un array
-  const arraySplit = userInput.split("");
-
-  //console.log(arraySplit);
+  const arraySplit = userInputLowerCase.split("");
 
   // Creo l'array della parola inserita ma con le lettere al contrario
   const arraySplitReverse = arraySplit.reverse();
@@ -17,13 +15,9 @@ function palindrome() {
   // Creo la parola con le lettere al contrario
   const reverseWord = arraySplitReverse.join("");
 
-  //console.log(arraySplitReverse);
-
-  //console.log(reverseWord);
-
   // Istruzione condizionale. Se le due parole sono identiche la parola è palindroma,
   // altrimenti non lo è
-  if (userInput === reverseWord) {
+  if (userInputLowerCase === reverseWord) {
     alert(`${"Ottimo!"} "${userInput}" ${"è una parola palindroma"}`);
   } else {
     alert(`${"Peccato!"} "${userInput}" ${"non è una parola palindroma"}`);
