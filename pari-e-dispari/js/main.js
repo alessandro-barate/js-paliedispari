9b1d526e0f87928126c877aca97866001b60a98c
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2024-05-07 21:45:08 +0200

    [UPD] Even odd exercise - control logic on the input number added

--- pari-e-dispari/js/main.js
+++ pari-e-dispari/js/main.js
@@ -1,8 +1,13 @@
 "use strict";
 
-const userEvenOdd = prompt("Scegli pari o dispari");
-const userNumber = Number(prompt("Scegli un numero da 1 a 5"));
+const userEvenOdd = prompt("Scegli pari o dispari").toLowerCase();
+let userNumber = Number(prompt("Scegli un numero da 1 a 5"));
 const pcNumber = randomNumber();
+
+while (userNumber > 5) {
+  alert("Non hai inserito un numero corretto");
+  userNumber = Number(prompt("Scegli un numero da 1 a 5"));
+}
 
 function randomNumber() {
   let random = Math.floor(Math.random() * 5 + 1);
@@ -12,3 +17,18 @@ function randomNumber() {
 console.log(userEvenOdd);
 console.log(userNumber);
 console.log(pcNumber);
+
+const numbersSum = userNumber + pcNumber;
+console.log(numbersSum);
+
+const result = evenOdd(numbersSum);
+
+function evenOdd() {
+  if (numbersSum % 2 === 0) {
+    console.log("Pari! Hai vinto");
+    //return true;
+  } else {
+    console.log("Dispari! Hai perso");
+    //return false;
+  }
+}
