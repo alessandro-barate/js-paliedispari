7daf64b0da7b6595a4ced557745c27115be7ffcb
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2024-05-07 17:42:52 +0200

    [UPD] Palindrome exercise done

--- palindroma/js/main.js
+++ palindroma/js/main.js
@@ -1,11 +1,29 @@
 "use strict";
 
+// Chiedo all'utente di inserire una parola
 const userInput = prompt("Inserisci una parola");
 
+//console.log(userInput);
+
+// Divido la parola nelle sue lettere e vengono messe in un array
 const arraySplit = userInput.split("");
 
-console.log(arraySplit);
+//console.log(arraySplit);
 
-arraySplit.reverse();
+// Creo l'array della parola inserita ma con le lettere al contrario
+const arraySplitReverse = arraySplit.reverse();
 
-console.log(arraySplit);
+// Creo la parola con le lettere al contrario
+const reverseWord = arraySplitReverse.join("");
+
+//console.log(arraySplitReverse);
+
+//console.log(reverseWord);
+
+// Istruzione condizionale. Se le due parole sono identiche la parola è palindroma,
+// altrimenti non lo è
+if (userInput === reverseWord) {
+  alert(`${"Ottimo!"} "${userInput}" ${"è una parola palindroma"}`);
+} else {
+  alert(`${"Peccato!"} "${userInput}" ${"non è una parola palindroma"}`);
+}
