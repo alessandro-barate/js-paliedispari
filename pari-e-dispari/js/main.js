0b1ae6a455e83b43cea764167c54859d0e9db2ea
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2024-05-07 22:00:18 +0200

    [UPD] Even odd exercise - controls on the inputs added - exercise done

--- pari-e-dispari/js/main.js
+++ pari-e-dispari/js/main.js
@@ -1,13 +1,20 @@
 "use strict";
 
-const userEvenOdd = prompt("Scegli pari o dispari").toLowerCase();
+let userEvenOdd = prompt("Scegli tra pari o dispari").toLowerCase();
+
+while (userEvenOdd !== "pari" && userEvenOdd !== "dispari") {
+  alert("Non hai scelto tra pari o dispari");
+  userEvenOdd = prompt("Scegli tra pari o dispari").toLowerCase();
+}
+
 let userNumber = Number(prompt("Scegli un numero da 1 a 5"));
-const pcNumber = randomNumber();
 
 while (userNumber > 5) {
   alert("Non hai inserito un numero corretto");
   userNumber = Number(prompt("Scegli un numero da 1 a 5"));
 }
+
+const pcNumber = randomNumber();
 
 function randomNumber() {
   let random = Math.floor(Math.random() * 5 + 1);
@@ -24,11 +31,15 @@ console.log(numbersSum);
 const result = evenOdd(numbersSum);
 
 function evenOdd() {
-  if (numbersSum % 2 === 0) {
+  if (numbersSum % 2 === 0 && userEvenOdd === "pari") {
     console.log("Pari! Hai vinto");
     //return true;
+  } else if (numbersSum % 2 === 0 && userEvenOdd === "dispari") {
+    console.log("Pari! Hai perso");
+    //return false;
+  } else if (numbersSum % 2 !== 0 && userEvenOdd === "dispari") {
+    console.log("Dispari! Hai vinto");
   } else {
     console.log("Dispari! Hai perso");
-    //return false;
   }
 }
