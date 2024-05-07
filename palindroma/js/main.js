d459d9b76e890e0449ad162251d231550e469916
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2024-05-07 20:25:45 +0200

    [UPD] Palindrome exercise changes

--- palindroma/js/main.js
+++ palindroma/js/main.js
@@ -3,26 +3,30 @@
 // Chiedo all'utente di inserire una parola
 const userInput = prompt("Inserisci una parola");
 
-function palindrome(userInput) {
-  // Controllo per trasformare l'input in lower case
-  let userInputLowerCase = userInput.toLowerCase();
+// Controllo per trasformare l'input in lower case
+let userInputLowerCase = userInput.toLowerCase();
 
+// Dichiaro la parola al contrario richiamando la funzione
+const reverseWord = palindrome(userInput);
+
+// Istruzione condizionale. Se le due parole sono identiche la parola è palindroma,
+// altrimenti non lo è
+if (userInputLowerCase === reverseWord) {
+  alert(`${"Ottimo!"} "${userInput}" ${"è una parola palindroma"}`);
+} else {
+  alert(`${"Peccato!"} "${userInput}" ${"non è una parola palindroma"}`);
+}
+
+// Funzione
+function palindrome(userInput) {
   // Divido la parola nelle sue lettere e vengono messe in un array
   const arraySplit = userInputLowerCase.split("");
 
   // Creo l'array della parola inserita ma con le lettere al contrario
   const arraySplitReverse = arraySplit.reverse();
 
-  // Creo la parola con le lettere al contrario
-  const reverseWord = arraySplitReverse.join("");
+  // Creo la stringa con le lettere al contrario
+  const strReverse = arraySplitReverse.join("");
 
-  // Istruzione condizionale. Se le due parole sono identiche la parola è palindroma,
-  // altrimenti non lo è
-  if (userInputLowerCase === reverseWord) {
-    alert(`${"Ottimo!"} "${userInput}" ${"è una parola palindroma"}`);
-  } else {
-    alert(`${"Peccato!"} "${userInput}" ${"non è una parola palindroma"}`);
-  }
+  return strReverse;
 }
-
-palindrome(userInput);
